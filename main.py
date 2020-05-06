218361bb3dd19da2d37f67b0e26287dcffd3bd14
MonocledMajesticProductions <46687573+MonocledMajesticProductions@users.noreply.github.com>
2020-05-06 20:41:17 +0100

    First Push
    Made the first insignifcant step . Actually started.

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+import os
+from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import send_file
+from flask import request
+from flask import make_response
+from flask import send_from_directory
+from flask import url_for
+from flask import jsonify
+
+from spotifyMethods import ApplicationVerification
+
+app = Flask(__name__)
+app.static_folder = "static"
+app.template_folder = "templates"
+
+
+@app.route("/callback")
+
+@app.route("/")#index
+def indexStart():
+    return render_template("index.html")
+
+@app.route("/Login/Spotify")
+def SpotifyLogIn():
+    print("connected")
+    return redirect(ApplicationVerification())
+    
+@app.route("/Login/Apple")
+def AppleLogIn():
+    return 0;
+@app.route("/Login/Google")
+def GoogleLogin():
+    return 0;
+
+if __name__ == "__main__":
+    debug = True
+    port = int(os.environ.get("PORT",5000))
+    app.run(host= '0.0.0.0',port=port,debug=debug)
---
+++ spotifyMethods.py
@@ -0,0 +1,19 @@
+import requests
+import urllib
+ClientID = "***REMOVED***"
+ClientSecret = "***REMOVED***"
+
+def ApplicationVerification():#https://developer.spotify.com/documentation/general/guides/authorization-guide/
+    auth_parameters ={
+        "client_id":"***REMOVED***",
+        "response_type" : "code",
+        "redirect_uri" : "http://127.0.0.1:5000/callback"#ok proof of redirect method to the bbc. cant go wrong.
+    }
+    #print("https://accounts.spotify.com/authorize?"+for)
+    print("https://accounts.spotify.com/authorize?"+urllib.parse.urlencode(auth_parameters))
+
+
+def GetAuthoristaionToken(): 
+    #current understanding is on user authorisation i receive a code , i then send this off to /api/token as a post request to get the code proper
+    return 0
+ApplicationVerification()
