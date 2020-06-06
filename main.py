e6c221b80901edd5e143b5685d881b5470be8a7b
MonocledMajesticProductions <46687573+MonocledMajesticProductions@users.noreply.github.com>
2020-06-07 00:43:28 +0100

    Web Server Production

--- main.py
+++ main.py
@@ -770,7 +770,9 @@ def ExceptionHandler(ExceptionData):
     return "s"
 
 if __name__ == "__main__":
-    debug = True
-    port = int(os.environ.get("PORT",5000))
-    app.run(host= '0.0.0.0',port=port,debug=debug)
+    from waitress import serve
+    serve(app,host= '0.0.0.0',port=port)
+    #debug = True
+    #port = int(os.environ.get("PORT",5000))
+    #app.run(host= '0.0.0.0',port=port,debug=debug)
     
