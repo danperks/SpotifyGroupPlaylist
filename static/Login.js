ef1c25ea2b3b0702215bea2ff41f7f85d7e17944
Dan Perks <49082234+danperks@users.noreply.github.com>
2020-06-18 20:32:57 +0100

    Latest updates

--- static/Login.js
+++ static/Login.js
@@ -1,46 +1,48 @@
-function Logout(){
-        
+function Logout() {
+
     document.cookie = "UserId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.cookie = "RefreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.cookie = "AuthToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location.replace("/");
 }
 
-function LeaveGroup(){
+function LeaveGroup() {
     GroupToLeave = document.getElementById("EnteredGroup").value;
-    $.get("/Management/AbandonGroup",{GroupCode:GroupToLeave}).done(function(){RefreshGroups()});
-        
+    $.get("/Management/AbandonGroup", { GroupCode: GroupToLeave }).done(function() { RefreshGroups() });
+
 }
-function Submission(){
-    sessionStorage.setItem("CurrentGroup",document.getElementById("EnteredGroup").value);//set current group
+
+function Submission() {
+    sessionStorage.setItem("CurrentGroup", document.getElementById("EnteredGroup").value); //set current group
 }
-function RefreshGroups(){
+
+function RefreshGroups() {
     $('#GroupTable').empty();
-    $.ajax("/api/UserGroups").done(function(data){
+    $.ajax("/api/UserGroups").done(function(data) {
         var table = document.getElementById("GroupTable")
         var header = table.insertRow();
-            var h1 = header.insertCell(0).innerHTML="Group Name".bold();
-            var h2 = header.insertCell(1).innerHTML="Group Code".bold();
-        console.log(data)
-        for(i =0;i<data[0].length;i++){
-            
+        var h1 = header.insertCell(0).innerHTML = "Group Name".bold();
+        var h2 = header.insertCell(1).innerHTML = "Group Code".bold();
+        console.log(data);
+        for (i = 0; i < data[0].length; i++) {
+
             var row = table.insertRow();
-            var cell1 = row.insertCell(0).innerHTML=data[1][i];
-            var cell2 = row.insertCell(1).innerHTML=data[0][i];
-            
+            var cell1 = row.insertCell(0).innerHTML = data[1][i];
+            var cell2 = row.insertCell(1).innerHTML = data[0][i];
+
         }
     })
 
-return "Table Updated"
+    return "Table Updated"
 }
-$(document).ready(RefreshGroups())
+$(document).ready(RefreshGroups());
 
-function CreateAGroup(){
-    DeafultUserName = String(Cookies.get("UserId")) + "'s Group"
-    GroupNameEntry = window.prompt("Enter A Group Name",DeafultUserName);
-    
-
-    $.post("/CreateGroup",{GroupName:GroupNameEntry}).done(function(data){
-        alert("Your New Group is " + String(data))
-    })
+function CreateAGroup() {
+    DeafultUserName = String(Cookies.get("UserId")) + "'s Group";
+    GroupNameEntry = window.prompt("Enter A Group Name", DeafultUserName);
+    if (GroupNameEntry) {
+        $.post("/CreateGroup", { GroupName: GroupNameEntry }).done(function(data) {
+            alert("Your New Group is " + String(data));
+        });
+    }
 }
--- static/VotingPage.js
+++ static/VotingPage.js
@@ -1,6 +1,3 @@
-"use strict";
-//jslint browser:true //
-
 var SongsToVoteOn = [];
 var VotesInFavour = [];
 var VotesAgainst = [];
@@ -14,159 +11,160 @@ var StateOfCheckbox = false;
 var CurrentAudioTrack = "";
 console.log(CurrentGroup);
 $(document).ready(function() {
-    $.get('/ReturnSongsAwaitVote',{GroupId:CurrentGroup}).done(function(data){
-        SongsToVoteOn=[];
+    $.get('/ReturnSongsAwaitVote', { GroupId: CurrentGroup }).done(function(data) {
+        SongsToVoteOn = [];
         CurrentSong = "";
-        SongsToVoteOn= data;
+        SongsToVoteOn = data;
         //console.log(data);
         //alert(SongsToVoteOn)
         NextSong();
-                //console.log(SongsToVoteOn);
+        //console.log(SongsToVoteOn);
         MakeButtonsLive();
         CheckBoxStateCheck();
-    }).fail(function(data){
+    }).fail(function(data) {
         alert("Your Session Has Likely Expired  - Redirecting to group selection page");
         window.location.replace("/");
     });
 });
 
-function MakeButtonsLive(){//probably should mess around with doing this propelry off a chain of promises / async funcitons etc, but acc that seems to work so , even if sketchy
-    if (SongsToVoteOn.length >0){
+function MakeButtonsLive() { //probably should mess around with doing this propelry off a chain of promises / async funcitons etc, but acc that seems to work so , even if sketchy
+    if (SongsToVoteOn.length > 0) {
         document.getElementById("AgainstButton").hidden = false;
         document.getElementById("InFavourButton").hidden = false;
         $("#AgainstButton").removeAttr("disabled");
         $("#InFavourButton").removeAttr("disabled");
-         //,false);
+        //,false);
         //$("#InFavourButton").prop("disabled",false);
         //alert(sessionStorage.getItem("CurrentGroup"));
-        
+
         //console.log(CurrentSong)
     }
-    RefreshOutputPlaylist()// refreshes output on user log in
+    RefreshOutputPlaylist() // refreshes output on user log in
 }
 
-function VoteInFavour(){
-    if(StateOfCheckbox){
+function VoteInFavour() {
+    if (StateOfCheckbox) {
         var GroupValueToSendBack = "ALL";
-    }
-    else{
+    } else {
         var GroupValueToSendBack = CurrentGroup;
     }
     console.log("Current Votes Applicable To " + GroupValueToSendBack.toString())
-    $.get('/VotesReturned',{InFavourVotes:JSON.stringify([CurrentSong]),GroupId:GroupValueToSendBack}).done(function(data){
+    $.get('/VotesReturned', { InFavourVotes: JSON.stringify([CurrentSong]), GroupId: GroupValueToSendBack }).done(function(data) {
         NextSong();
     })
 }
 
-    
 
 
 
-function VoteAgainst(){
-    if(StateOfCheckbox){
+
+function VoteAgainst() {
+    if (StateOfCheckbox) {
         var GroupValueToSendBack = "ALL";
-    }
-    else{
+    } else {
         var GroupValueToSendBack = CurrentGroup;
     }
     console.log("Current Votes Applicable To " + GroupValueToSendBack.toString())
     document.getElementById("PreviewPlayer").pause();
-    $.get('/VotesReturned',{VotesAgainst:JSON.stringify([CurrentSong]),GroupId:GroupValueToSendBack}).done(function(data){
+    $.get('/VotesReturned', { VotesAgainst: JSON.stringify([CurrentSong]), GroupId: GroupValueToSendBack }).done(function(data) {
         NextSong();
     })
 }
-function NextSong(){
+
+function NextSong() {
     //console.log(SongsToVoteOn)
     $("#PreviewPlayer").hide();
     //console.log("Next Song Called");
-    if (SongsToVoteOn && SongsToVoteOn.length >0){
+    if (SongsToVoteOn && SongsToVoteOn.length > 0) {
         CurrentSong = SongsToVoteOn.pop();
         document.getElementById("VoteCount").innerHTML = String(SongsToVoteOn.length);
         SetAlbumImage(CurrentSong);
         GetThirtySecondAudio(CurrentSong);
-    }
-    else{
+    } else {
         //Now at end of the list
         PackUpSendBack();
     }
     // console.log(SongsToVoteOnTwo)
-    
+
 }
-function SetAlbumImage(SongID){
+
+function SetAlbumImage(SongID) {
     $.ajax({
-        url:"https://api.spotify.com/v1/tracks/"+ String(SongID),
-        headers:{
+        url: "https://api.spotify.com/v1/tracks/" + String(SongID),
+        headers: {
             'Authorization': 'Bearer ' + String(Cookies.get("AuthToken")),
         },
-        success:function(response){
+        success: function(response) {
             document.getElementById("AlbumCover").src = response["album"]["images"][0]["url"];
-            document.getElementById("Artist").innerHTML = response["artists"][0]["name"];// will only get first artist but you know where to go with that
+            document.getElementById("Artist").innerHTML = response["artists"][0]["name"]; // will only get first artist but you know where to go with that
             document.getElementById("Title").innerHTML = response["name"]
-           // alert(Cookies.get("AuthToken"))
+                // alert(Cookies.get("AuthToken"))
         },
-        fail:function(response){
-            console.log("Failure : "+String(response));
+        fail: function(response) {
+            console.log("Failure : " + String(response));
         }
     })
 }
-function GetThirtySecondAudio(SongID){
+
+function GetThirtySecondAudio(SongID) {
     $.ajax({
-        url:"https://api.spotify.com/v1/tracks/"+ String(SongID)+"?market=from_token",
-        headers:{
+        url: "https://api.spotify.com/v1/tracks/" + String(SongID) + "?market=from_token",
+        headers: {
             'Authorization': 'Bearer ' + String(Cookies.get("AuthToken")),
         },
-        success:function(response){
-            console.log(response)
-            CurrentAudioTrack = response["preview_url"]
-            if(CurrentAudioTrack == null){
+        success: function(response) {
+            console.log(response);
+            CurrentAudioTrack = response["preview_url"];
+            if (CurrentAudioTrack == null) {
                 $("#PreviewPlayer").hide();
-            }
-            else{
+            } else {
                 $("#PreviewPlayer").show();
                 document.getElementById("PreviewPlayer").src = CurrentAudioTrack;
             }
         },
-        fail:function(response){
-            console.log("Failure : "+String(response));
+        fail: function(response) {
+            console.log("Failure : " + String(response));
         }
 
-        })
-           
-           // alert(Cookies.get("AuthToken"))
-        }
+    })
 
-    
+    // alert(Cookies.get("AuthToken"))
+}
 
 
-function PackUpSendBack(){
+
+
+function PackUpSendBack() {
     //alert("End Of List Reached");
     HideElements();
     RefreshOutputPlaylist();
     alert("End of Songs - Check back later to see if any more songs have been submitted");
     // add the check to see if all votes have been received
-     
+
 }
-function CheckBoxStateCheck(){
+
+function CheckBoxStateCheck() {
     StateOfCheckbox = document.getElementById("VotesPermanent").checked;
     //alert(StateOfCheckbox);
     console.log(StateOfCheckbox);
 }
-function HideElements(){
+
+function HideElements() {
     $('#divHide').children().hide();
-    
+
 }
 
-function RefreshOutputPlaylist(){
-    $.get("/RefreshOutputPlaylist",{GroupId:CurrentGroup}).done(function(){
+function RefreshOutputPlaylist() {
+    $.get("/RefreshOutputPlaylist", { GroupId: CurrentGroup }).done(function() {
         console.log("Output Playlist Refreshed");
     })
 }
 
 
-    // Both Arrays combined into JSON String wdetailing what each array is
-    //field for group added
-    //field for user id added
-    //string sent back up to the flask
-    //string unpacked on the flask
-    //array of negative votes is then porcessed (simply just record each vote on the database)
-    //arrray of positive votes is then done in the same manner
+// Both Arrays combined into JSON String wdetailing what each array is
+//field for group added
+//field for user id added
+//string sent back up to the flask
+//string unpacked on the flask
+//array of negative votes is then porcessed (simply just record each vote on the database)
+//arrray of positive votes is then done in the same manner
